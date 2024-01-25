70e95c978ea8ac8afcbfbbdbcb98a8f0c0c655ef
Hanzla Saadi <hanzalasaadi@gmail.com>
2024-01-26 02:54:13 +0500

    filter solo page/route added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import ScrollToTop from "./ScrollToTop";
 import MainPage from "./Sub_Component/Dashboard/MainPage";
 import AddCategory from "./Sub_Component/Dashboard/AddCategory";
 import AddProduct from "./Sub_Component/Dashboard/AddProduct";
+import AddFilter from "./Sub_Component/Dashboard/AddFilter";
 import Orders from "./Sub_Component/Dashboard/Orders";
 import AllProducts from "./Sub_Component/Dashboard/AllProducts";
 import AllCategory from "./Sub_Component/Dashboard/AllCategory";
@@ -19,6 +20,7 @@ const App = () => {
             <Route path="/dashboard/addCategory" element={<AddCategory />} />
             <Route path="/dashboard/all_category" element={<AllCategory />} />
             <Route path="/dashboard/addProduct" element={<AddProduct />} />
+            <Route path="/dashboard/addFilter" element={<AddFilter />} />
             <Route path="/dashboard/all_product" element={<AllProducts />} />
             <Route path="/dashboard/orders" element={<Orders />} />
           </Routes>
---
+++ src/Sub_Component/Dashboard/AddFilter.js
@@ -0,0 +1,234 @@
+import React from "react";
+import DashboardNavbar from "./DashboardNavbar";
+import { Container, Row, Col } from "react-bootstrap";
+import Form from "react-bootstrap/Form";
+import { RiDeleteBinLine } from "react-icons/ri";
+
+import { FiEdit } from "react-icons/fi";
+
+function AddFilter() {
+  return (
+    <div>
+      <DashboardNavbar />
+      <div>
+        <div class=" mt-24 absolute lg:left-[260px] z-5">
+          <Container class="">
+            <h2 class="font-bold text-xl">Add Filter</h2>
+            <Row class="">
+              <Col md={4}>
+                <Form.Group as={Col} controlId="" sm={4} className=" mt-3">
+                  <Form.Label class="text-[#707070]  font-semibold py-2 "></Form.Label>
+                  <Form.Control
+                    type="file"
+                    style={{ height: "170px", width: "278px" }}
+                    accept="image/png, image/jpeg"
+                  />
+                  <div class="absolute text-center " style={{ marginTop: -94 }}>
+                    {/* <p class=" text-xl px-36 text-[#707070]">
+                      <FiEdit />
+                      hello
+                    </p> */}
+                    <button
+                      class="rounded-1 p-2 bg-[#1B94A0] text-white"
+                      style={{ marginTop: 37 }}
+                    >
+                      Upload Icon
+                    </button>
+                  </div>
+                  {/* <p
+                    class="absolute text-center px-24"
+                    style={{ marginTop: -66 }}
+                  >
+                    choose an image
+                  </p> */}
+                </Form.Group>
+                {/* <Row>
+                  <Col>
+                    <Form.Group as={Col} controlId="" sm={4} className="">
+                      <Form.Label class="text-[#707070] font-semibold py-2"></Form.Label>
+                      <Form.Control
+                        type="text"
+                        style={{ height: "40px", width: "45px" }}
+                      />
+                      <div
+                        class="absolute text-center "
+                        style={{ marginTop: -28 }}
+                      >
+                        <p class=" px-3 text-[#707070]">
+                          <FiEdit />
+                        </p>
+                      </div>
+                    </Form.Group>
+                  </Col>
+
+                  <Col>
+                    <Form.Group as={Col} controlId="" sm={4} className="">
+                      <Form.Label class="text-[#707070] font-semibold py-2"></Form.Label>
+                      <Form.Control
+                        type="text"
+                        style={{ height: "40px", width: "45px" }}
+                      />
+                      <div
+                        class="absolute text-center "
+                        style={{ marginTop: -28 }}
+                      >
+                        <p class=" px-3 text-[#707070]">
+                          <FiEdit />
+                        </p>
+                      </div>
+                    </Form.Group>
+                  </Col>
+
+                  <Col>
+                    <Form.Group as={Col} controlId="" sm={4} className="">
+                      <Form.Label class="text-[#707070] font-semibold py-2"></Form.Label>
+                      <Form.Control
+                        type="text"
+                        style={{ height: "40px", width: "45px" }}
+                      />
+                      <div
+                        class="absolute text-center "
+                        style={{ marginTop: -28 }}
+                      >
+                        <p class=" px-3 text-[#707070]">
+                          <FiEdit />
+                        </p>
+                      </div>
+                    </Form.Group>
+                  </Col>
+
+                  <Col>
+                    <Form.Group as={Col} controlId="" sm={4} className="">
+                      <Form.Label class="text-[#707070] font-semibold py-2"></Form.Label>
+                      <Form.Control
+                        type="text"
+                        style={{ height: "40px", width: "45px" }}
+                      />
+                      <div
+                        class="absolute text-center "
+                        style={{ marginTop: -28 }}
+                      >
+                        <p class=" px-3 text-[#707070]">
+                          <FiEdit />
+                        </p>
+                      </div>
+                    </Form.Group>
+                  </Col>
+                </Row> */}
+              </Col>
+
+              <Col md={8}>
+                <Form>
+                  <Row className="mb-3 lg:px-16 mt-3">
+                    <Form.Group as={Col} controlId="">
+                      <Form.Label class="text-[#707070] font-semibold py-2">
+                        Select Category
+                      </Form.Label>
+                      <Form.Select aria-label="Default select example">
+                        <option value={0}>Zero</option>
+                        <option value="1">One</option>
+                        <option value="2">Two</option>
+                        <option value="3">Three</option>
+                      </Form.Select>
+                    </Form.Group>
+
+                    <Form.Group as={Col} controlId="">
+                      <Form.Label class="text-[#707070]  font-semibold py-2">
+                        Name
+                      </Form.Label>
+                      <Form.Control type="text" />
+                    </Form.Group>
+
+                    <Form.Group as={Col} controlId="">
+                      <Form.Label class="text-[#707070]  font-semibold py-2">
+                        Alternate Name
+                      </Form.Label>
+                      <Form.Control type="text" />
+                    </Form.Group>
+                  </Row>
+
+                  <Row className="mb-3 lg:px-16 mt-3">
+                    <Form.Group as={Col} controlId="">
+                      <Form.Label class="text-[#707070]  font-semibold py-2">
+                        Priority Number
+                      </Form.Label>
+                      <Form.Control type="text" />
+                    </Form.Group>
+
+                    <Form.Group as={Col} controlId="">
+                      <Form.Label class="text-[#707070] font-semibold py-2">
+                        Show in Navbar
+                      </Form.Label>
+                      <Form.Select aria-label="Default select example">
+                        <option value={true}>Yes</option>
+                        <option value={false}>No</option>
+                      </Form.Select>
+                    </Form.Group>
+
+                    <Form.Group as={Col} controlId="">
+                      <Form.Label class="text-[#707070] font-semibold py-2">
+                        Show in Filterbar
+                      </Form.Label>
+                      <Form.Select aria-label="Default select example">
+                        <option value={true}>Yes</option>
+                        <option value={false}>No</option>
+                      </Form.Select>
+                    </Form.Group>
+                  </Row>
+
+                  <Row className="mb-3 lg:px-16 mt-3">
+                    <Form.Group as={Col} controlId="" md={2}>
+                      <Form.Label class="text-[#707070]  font-semibold py-2">
+                        Add New Options
+                      </Form.Label>
+                      <Form.Control type="text" />
+                    </Form.Group>
+                    <Form.Group as={Col} controlId="">
+                      <button
+                        class="rounded-1 p-2 bg-[#1B94A0] text-white"
+                        style={{ marginTop: 37 }}
+                      >
+                        Add Variant
+                      </button>
+                    </Form.Group>
+
+                    <Form.Group
+                      as={Col}
+                      controlId=""
+                      md={4}
+                      style={{ marginTop: 15 }}
+                    >
+                      <Form.Label class="text-[#707070]  font-semibold py-2">
+                        Current Options
+                      </Form.Label>
+                      <Form.Select aria-label="Default select example">
+                        <option></option>
+                        <option value="1">One</option>
+                        <option value="2">Two</option>
+                        <option value="3">Three</option>
+                      </Form.Select>
+                    </Form.Group>
+                    <Form.Group as={Col} controlId="">
+                      <button
+                        class="rounded-1 p-2  text-white"
+                        style={{ marginTop: 37 }}
+                      >
+                        {" "}
+                        <RiDeleteBinLine class="text-[#707070] text-2xl" />
+                      </button>
+                    </Form.Group>
+                  </Row>
+                </Form>
+              </Col>
+            </Row>
+            <button class="rounded-1 p-2 w-32 font-semibold   mt-4 bg-[#1B94A0] text-white">
+              Submit
+            </button>
+          </Container>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AddFilter;
